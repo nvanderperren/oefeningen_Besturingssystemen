ac5e8141ec96d93e62241d42f40f959f93ba8f3f
nvanderperren <ik.ben.nas@gmail.com>
2019-03-05 12:17:24 +0100

    change deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+#
+# Author: Nastasia Vanderperren (PACKED/VIAA)
+# Goal: script to deploy the CultURIze files to the webserver
+#
+##############################################
+
 cd ~/varia
+
+echo "start git"
 git reset --hard
 git pull
 git checkout master
-cd /var/www/html
-cp -r ~/varia/* .
+
+echo "start copying files to webserver"
+cp -r * /var/www/html/
+if [ -f .htaccss ]; then
+    cp .htaccess /var/www/html/
+fi
+
+echo "script finished"
